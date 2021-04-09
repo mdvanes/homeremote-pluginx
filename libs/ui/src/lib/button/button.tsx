b8bd3451165787a7e332264d55f57fb050bd0611
M.D. van Es <es@mdworld.nl>
2021-04-09 15:37:16 +0200

    style button

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -6,9 +6,16 @@ export interface ButtonProps {
 }
 
 const StyledButton = styled.button`
-background-color: blue;
+  background-color: #1976d2;
   border: none;
+  border-radius: 0.2rem;
+  color: white;
+  padding: 0.4rem;
   text-transform: uppercase;
+  cursor: pointer;
+  font-weight: bold;
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+  font-size: 0.875rem;
 `;
 
 export function Button({ label }: ButtonProps) {
