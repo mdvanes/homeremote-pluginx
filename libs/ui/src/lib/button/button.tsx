8257186ccf3a595e837134820b7d7434fa584677
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2021-04-09 15:56:39 +0200

    style button

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export interface ButtonProps {
   label: string;
+  onClick?: () => void;
 }
 
 const StyledButton = styled.button`
@@ -14,16 +15,17 @@ const StyledButton = styled.button`
   text-transform: uppercase;
   cursor: pointer;
   font-weight: bold;
-  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
+    0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
   font-size: 0.875rem;
+
+  &:hover {
+    background-color: rgb(17, 82, 147);
+  }
 `;
 
-export function Button({ label }: ButtonProps) {
-  return (
-    <StyledButton onClick={() => alert('Not yet implemented!')}>
-      {label}
-    </StyledButton>
-  );
+export function Button({ label, onClick = () => {} }: ButtonProps) {
+  return <StyledButton onClick={onClick}>{label}</StyledButton>;
 }
 
 export default Button;
