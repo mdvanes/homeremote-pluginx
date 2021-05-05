26b10c585c79b7f17a997cc3a30567c0c9fb9940
M.D. van Es <es@mdworld.nl>
2021-05-05 16:27:26 +0200

    intial

---
+++ apps/homeremote/webpack.config.js
@@ -0,0 +1,46 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.base.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "homeremote"
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      
+        // For remotes (please adjust)
+        // name: "homeremote",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './apps/homeremote/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        // remotes: {
+        //     "app": "app@http://localhost:4200/remoteEntry.js",
+        //     "stream-player": "stream-player@http://localhost:4200/remoteEntry.js",
+
+        // },
+
+        shared: {
+          "@angular/core": { singleton: true, strictVersion: true }, 
+          "@angular/common": { singleton: true, strictVersion: true }, 
+          "@angular/router": { singleton: true, strictVersion: true },
+
+          ...sharedMappings.getDescriptors()
+        }
+        
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
